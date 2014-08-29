78da053971865c61c043f9632e3f78482012ae55
trillobite <jparnell0@gmail.com>
2014-08-29 22:13:20 +0000

    cleaned up code

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,18 @@
 
 var templates = {
-	textDiv: function(textArray) {
-		console.log(textArray);
-		var text = "";
-		$.each(textArray, function() {
-			text += '<p>' + this + '</p>';
-		});
-
+	textDiv: function(rawData, id) {
+		var text = project.parseRaw(rawData);
 		return $jConstruct('div', {
-			class: 'textLeft',
-			text: text,
+		    id: id,
+		    class: 'textLeft',
+		    text: text, 
 		});
 	},
-
-	overviewFile: function() {
-		return $jConstruct('iframe', {
-			id: 'overviewFile',
-			src: 'scripts/text/overview.txt',
-
-		}).css({
-			'display': 'none',
-		});
-	},
-
-	fileToHTML: function(file, id) {
-		return $jConstruct('iframe', {
-			id: id,
-			//class: 'dataBox',
-			src: file,
-			onload: "project.stuff('" + id + "');",
-		});
-	},
-
+    
+    docBody: $jConstruct('div', {
+        id: 'docBody',
+        text: '<h1>jsonHTML</h1>',
+    }),
 };
 
 var project = {
@@ -42,23 +23,17 @@ var project = {
 	    });
 	    return dfd.promise;
     },
+    parseRaw: function(raw) {
+        raw = raw.replace(/(\r\n|\n|\r)/gm, '</p><p>');
+        raw = '<p>' + raw + '</p>';
+        return raw;
+    },
 };
 
 $(document).ready(function() {
-	var main = $jConstruct('div', {
-		id: 'docBody',
-		text: '<h1>jsonHTML</h1>',
-	});
-	
+	var main = templates.docBody;
 	project.getData('overview.txt')().done(function(data) {
-	    data = data.replace(/(\r\n|\n|\r)/gm, '</p><p>');
-	    data = '<p>' + data + '</p>';
-	    console.log(data);
-	    main.addChild($jConstruct('div', {
-	        class: 'textLeft',
-	        text: data,
-	    }));
-	    console.log(main);
+	    main.addChild(templates.textDiv(data, 'textField'));
 	    main.appendTo('body');
 	});
 });
