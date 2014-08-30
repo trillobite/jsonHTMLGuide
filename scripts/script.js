6b31af1f5c449832e68de8cf02b327368a3e9942
trillobite <jparnell0@gmail.com>
2014-08-30 05:57:50 +0000

    appears to be finished surprisingly

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 
 var templates = {
 	textDiv: function(rawData, id) {
-		var text = project.parseRaw(rawData);
 		return $jConstruct('div', {
 		    id: id,
 		    class: 'textLeft',
-		    text: text, 
+		    text: rawData, 
 		});
 	},
     
@@ -14,11 +13,106 @@ var templates = {
         text: '<h1>jsonHTML</h1>',
     }),
     
-    navigation: function() {
-        return $jConstruct('div', {
+    navigation: function(tutorialBody) {
+        var changeText = function(filePath) {
+            project.getData(filePath)().done(function(data) {
+                tutorialBody.text = data;
+                tutorialBody.refresh();
+            });
+        };
+        var navObjects = [
+            $jConstruct('div', {
+                id: 'navOverview',
+                text: '<h5>Introduction</h5>',
+            }).event('click', function() {
+                changeText('scripts/text/overview.txt');
+            }).css({
+                'background-color': '#d7d7d6',
+            }),
+            
+            $jConstruct('div', {
+                id: 'navUsage',
+                text: '<h5>The Basics</h5>',
+            }).event('click', function() {
+            	changeText('scripts/text/usage.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navAppending',
+                text: '<h5>Appending Objects</h5>'
+            }).event('click', function() {
+                changeText('scripts/text/appending.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navStyling',
+                text: '<h5>Styling Objects</h5>'
+            }).event('click', function() {
+                changeText('scripts/text/styling.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navAddChild',
+                text: '<h5>Adding Child Objects</h5>',
+            }).event('click', function() {
+                changeText('scripts/text/children.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navUtilize',
+                text: '<h5>Utilizing Your Database</h5>',
+            }).event('click', function() {
+                changeText('scripts/text/usingDb.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navEvents',
+                text: '<h5>Adding Events</h5>',
+            }).event('click', function() {
+                changeText('scripts/text/eventing.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navRefresh',
+                text: '<h5>Refreshing Your Objects</h5>',
+            }).event('click', function() {
+                changeText('scripts/text/refresh.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navExperimental',
+                text: '<h5>Experimental Exploits</h5>',
+            }).event('click', function() {
+                changeText('scripts/text/experimental.txt');
+            }),
+            
+            $jConstruct('div', {
+                id: 'navDownload',
+                text: '<h5>Download jsonHTML<h5>',
+            }).event('click', function() {
+                changeText('scripts/text/download.txt');
+            }),
+        ];
+        
+        var navigation = $jConstruct('div', {
             id: 'nav',
             text: '<h3>Navigation</h3>',
         });
+        
+        $.each(navObjects, function(indx, obj) { //make sure the nav keeps track of where it's at!
+            obj.class = 'navigationButton';
+            obj.event('click', function() {
+                $('.navigationButton').each(function() {
+                    this.style['background-color'] = 'white';
+                });
+                $('#'+obj.id).css({
+                    'background-color': '#d7d7d6',
+                });
+            })
+            navigation.addChild(obj);
+        });
+        
+        return navigation;
     },
     
     container: function() {
@@ -37,20 +131,16 @@ var project = {
 	    });
 	    return dfd.promise;
     },
-    parseRaw: function(raw) {
-        raw = raw.replace(/(\r\n|\n|\r)/gm, '</p><p>');
-        raw = '<p>' + raw + '</p>';
-        return raw;
-    },
 };
 
 $(document).ready(function() {
     var container = templates.container();
 	var main = templates.docBody;
-	project.getData('overview.txt')().done(function(data) {
-	    main.addChild(templates.textDiv(data, 'textField'));
+	project.getData('scripts/text/overview.txt')().done(function(data) {
+	    var textField = templates.textDiv(data, 'textField');
+	    main.addChild(textField);
 	    container.addChild(main);
-	    container.addChild(templates.navigation());
+	    container.addChild(templates.navigation(textField));
 	    container.appendTo('body');
 	});
 	
